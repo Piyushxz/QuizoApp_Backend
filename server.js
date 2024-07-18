20c5cfd7c11975d6cfc18d9c93cf1090c7534b54
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-07-18 18:48:03 +0530

    userlogin route and jwt logic

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const cors = require("cors")
 const app = express()
 const quizRouter = require("./router/quiz.router")
+const userdata = require("./db/users")
+const jwt = require('jsonwebtoken')
+require('dotenv').config(); // Load environment variables from .env file
 
 
 const PORT = 3001;
@@ -13,6 +16,18 @@ app.get("/",(req,res)=>{
     res.send("Hello WOrld");
 })
 
+app.post("/auth/login",(req,res)=>{
+    const {username,password} = req.body;
+    const isUserVerified = userdata.users.some(user => user.username === username && user.password === password);
+
+    if(isUserVerified){
+        const token = jwt.sign({id:username},process.env.SECRET_TOKEN)
+
+        res.json({username,token,message:"Valid User"})
+    }else{
+        res.status(401).send("Invalid user")
+    }
+})
 // app.get("/quiz",(req,res)=>{
 //     res.send(quizzes.data);
 // })
