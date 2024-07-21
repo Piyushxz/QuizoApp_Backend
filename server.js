597f36c5006645d14f3e6045b861132b8767774f
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-07-21 11:53:56 +0530

    added category router

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const quizRouter = require("./router/quiz.router")
 const {loginRouter,signUpRouter} = require("./router/auth.router")
 const routeNotFound = require("./middlerware/routeNotFound")
+const categoryRouter = require("./router/category.router")
 const quizzes = require('./db/quizzes')
 
 require('dotenv').config(); // Load environment variables from .env file
@@ -13,15 +14,13 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send(quizzes);
-})
+
 
 
 // app.get("/quiz",(req,res)=>{
 //     res.send(quizzes.data);
 // })
-
+app.use("/categories",categoryRouter)
 app.use("/quiz",quizRouter);
 app.use("/auth/login",loginRouter);
 app.use("/auth/signup",signUpRouter);
